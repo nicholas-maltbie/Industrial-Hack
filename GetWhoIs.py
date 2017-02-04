2063783b476c9ac2a287f7f72f1d474ab49f9024
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-02-04 05:29:55 -0500

    Added whois scanning

--- GetWhoIs.py
+++ GetWhoIs.py
@@ -1,5 +1,6 @@
 """This will use the Censys data to determine information concerning the 
-owner of different ip addresses"""
+owner of different ip addresses. This file requires a API ID and Secret from a
+Censys account and these must be in a file 'censys' (no extension)."""
 import censys
 import json
 import requests
@@ -8,7 +9,7 @@ def get_who_is(ip_address):
     """Finds the location (long, latt) of an ip_address using censys"""
     data = json.loads(open('censyskey', 'r').read())
 
-    API_URL = "https://censys.io/ipv4/166.251.70.56/rawwhois/_data"
+    API_URL = "https://censys.io/ipv4/" + ip_address + "/rawwhois/_data"
     UID = data["API ID"]
     SECRET = data["Secret"]
 
@@ -17,5 +18,24 @@ def get_who_is(ip_address):
     #if res.status_code == 200:
     #    content = res.content.decode('utf-8')
     #    loaded = json.loads(content)
-    return res;
-        
+    
+    a = res.content.decode('utf-8').replace('\n', '').replace('&#34', '')
+    a = a[a.index('{') : len(a) - a[::-1].index('}')]
+    a = a.replace(';', '"')
+    return json.loads(a);
+
+def get_contacts(whois_data):
+    """parse all dictionaries that are mapped by the word 'contact'"""
+    found = []
+    for key in whois_data:
+        if key == "contact":
+            found.append(whois_data[key])
+        elif type(whois_data[key]) == dict:
+            found.extend(get_contacts(whois_data[key]))
+    return found
+
+def get_whois(ip_address):
+    """Given an ip address, this will search for all whois information tied to 
+    this address."""
+    return get_contacts(get_who_is(ip_address))
+    
