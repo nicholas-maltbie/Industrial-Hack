4250e730bcc4cf630eea9a03cb95e0f8f3d35f16
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-02-04 00:35:10 -0500

    Added default gui to control panel

--- controller.py
+++ controller.py
@@ -1,1 +1,29 @@
 import tkinter as tk
+
+class ControlPanel(tk.Frame):
+    """A control panel is a Grapical User Interface for a user to interact with
+    the rest of the project."""
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ControlPanel(master=root)
+    app.mainloop()
+
