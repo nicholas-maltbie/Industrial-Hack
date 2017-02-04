006be7fe4c9e0b3af959d16cfd3bd824a4bac5a3
davistj7 <davistj7@miamioh.edu>
2017-02-04 02:49:20 -0500

    label working but not showing up

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 
 class ControlPanel(tk.Frame):
     """A control panel is a Grapical User Interface for a user to interact with
@@ -11,41 +12,47 @@ class ControlPanel(tk.Frame):
 # ips, owner, filter, location, auto
     def create_widgets(self):
 
-        self.textarea = tk.Button(self)
-        self.textarea.grid(column=0, row=0, columnspan=4)
+##        self.textarea = tk.Button(None)
+##        self.textarea.grid(column=0, row=0, columnspan=4)
+##
+##        self.test = tk.Button(None)
+##        self.test.grid(column=1, row=1, columnspan=4)
+        
+        self.ips = tk.Button(None)
+        self.ips["text"] = "Get IP's"
+        self.ips["command"] = self.get_ips
+        #self.ips.pack(side="top")
+        #Label(master, text="Test").grid(row=0)
+        Label(self, text = "asdfasdfasdf").grid(column=0, row=0)
+        self.ips.grid(row = 0, column = 1)
 
-        self.test = tk.Button(self)
-        self.test.grid(column=1, row=1, columnspan=4)
-##        self.ips = tk.Button(self)
-##        self.ips["text"] = "Get IP's"
-##        self.ips["command"] = self.get_ips
-##        #self.ips.pack(side="top")
-##        #Label(master, text="Test").grid(row=0)
-##        self.ips.grid(row = 0, column = 0)
-##
-##        self.location = tk.Button(self)
-##        self.location["text"] = "Get Location"
-##        self.location["command"] = self.find_location
-##        #self.location.pack(side="top")
-##
-##        self.filter = tk.Button(self)
-##        self.filter["text"] = "Filter Data"
-##        self.filter["command"] = self.filter_data
-##        #self.filter.pack(side="top")
-##
-##        self.owner = tk.Button(self)
-##        self.owner["text"] = "Find Owner"
-##        self.owner["command"] = self.find_owner
-##        #self.owner.pack(side="top")
-##
-##        self.auto = tk.Button(self)
-##        self.auto["text"] = "Auto Generate"
-##        self.auto["command"] = self.auto
-##        #self.auto.pack(side="top")
-##        
-##        self.quit = tk.Button(self, text="QUIT", fg="red",
-##                              command=root.destroy)
-##        #self.quit.pack(side="bottom")
+        self.location = tk.Button(None)
+        self.location["text"] = "Get Location"
+        self.location["command"] = self.find_location
+        #self.location.pack(side="top")
+        self.location.grid(row = 1, column = 0)
+
+        self.filter = tk.Button(None)
+        self.filter["text"] = "Filter Data"
+        self.filter["command"] = self.filter_data
+        #self.filter.pack(side="top")
+        self.filter.grid(row = 2, column = 0)
+
+        self.owner = tk.Button(None)
+        self.owner["text"] = "Find Owner"
+        self.owner["command"] = self.find_owner
+        #self.owner.pack(side="top")
+        self.owner.grid(row = 3, column = 0)
+
+        self.auto = tk.Button(None)
+        self.auto["text"] = "Auto Generate"
+        self.auto["command"] = self.auto
+        #self.auto.pack(side="top")
+        self.auto.grid(row = 4,  column = 0)
+        
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        #self.quit.pack(side="bottom")
 
     def get_ips(self):
         print("Getting IP's...")
