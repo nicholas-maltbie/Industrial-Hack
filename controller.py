ec098e870e1066043580759be6dc0029929d1d90
davistj7 <davistj7@miamioh.edu>
2017-02-04 01:18:38 -0500

    basic gui

--- controller.py
+++ controller.py
@@ -7,23 +7,62 @@ class ControlPanel(tk.Frame):
         super().__init__(master)
         self.pack()
         self.create_widgets()
-
+        
+# ips, owner, filter, location, auto
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.ips = tk.Button(self)
+        self.ips["text"] = "Get IP's"
+        self.ips["command"] = self.get_ips
+        self.ips.pack(side="top")
 
+        self.location = tk.Button(self)
+        self.location["text"] = "Get Location"
+        self.location["command"] = self.find_location
+        self.location.pack(side="top")
+
+        self.filter = tk.Button(self)
+        self.filter["text"] = "Filter Data"
+        self.filter["command"] = self.filter_data
+        self.filter.pack(side="top")
+
+        self.owner = tk.Button(self)
+        self.owner["text"] = "Find Owner"
+        self.owner["command"] = self.find_owner
+        self.owner.pack(side="top")
+
+        self.auto = tk.Button(self)
+        self.auto["text"] = "Auto Generate"
+        self.auto["command"] = self.auto
+        self.auto.pack(side="top")
+        
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=root.destroy)
         self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def get_ips(self):
+        print("Getting IP's...")
 
+        print("Done!")
+    def find_location(self):
+        print("Finding Location...")
+
+        print("Done!")
+    def filter_data(self):
+        print("Filtering...")
+
+        print("Done!")
+    def find_owner(self):
+        print("Finding owner...")
+
+        print("Done!")
+    def auto(self):
+        self.get_ips(self)
+        self.find_location(self)
+        self.filter_data(self)
+        self.find_owner(self)
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = ControlPanel(master=root)
     app.mainloop()
-
